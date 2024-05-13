12af42b3d35d3581f7848aeb1e36f30c3792f173
burakhanunver <burakhanunver@gmail.com>
2024-05-13 12:55:30 +0300

    remove temp resume data

--- src/pages/PdfResult.tsx
+++ src/pages/PdfResult.tsx
@@ -2,95 +2,11 @@ import { PDFViewer, PDFDownloadLink } from "@react-pdf/renderer"
 import Pdf1 from "@/components/Pdf1"
 import resumeStore from "@/store"
 import { Link } from "react-router-dom"
+import { mockResume } from "@/lib/mockResumeData"
 
 function PdfResult() {
-  let resume = resumeStore((state) => state.resume)
-  console.log("Current Resume : ", resume)
-  const tempResume: typeof resume = {
-    skills: [
-      { isShow: true, name: "Javascript" },
-      { isShow: true, name: "Javascript" },
-      { isShow: true, name: "Javascript" },
-      { isShow: true, name: "Javascript" },
-      { isShow: true, name: "Javascript" },
-    ],
-    title: "Computer Engineer",
-    firstName: "BurakBurak",
-    lastName: "UnverUnver",
-    email: "burakhanunver@gmail.com",
-    tel: "9555 555 55 55",
-    about:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ratione, eligendi consequuntur sequi quas consectetur ipsam? Dolor vitae odio ad.",
-    educations: [
-      {
-        isShow: true,
-        city: "London",
-        degree: "Bachelor's Degree",
-        endMonth: "June",
-        endYear: "2025",
-        schoolName: "New york state university",
-        startMonth: "July",
-        startYear: "2021",
-      },
-      {
-        isShow: true,
-        city: "London",
-        degree: "Bachelor's Degree",
-        endMonth: "June",
-        endYear: "2025",
-        schoolName: "New york state university",
-        startMonth: "July",
-        startYear: "2021",
-      },
-      {
-        isShow: true,
-        city: "London",
-        degree: "Bachelor's Degree",
-        endMonth: "June",
-        endYear: "2025",
-        schoolName: "New york state university",
-        startMonth: "July",
-        startYear: "2021",
-      },
-    ],
-    jobs: [
-      {
-        isShow: true,
-        city: "New york",
-        company: "Acme company",
-        endMonth: "December",
-        endYear: "2026",
-        startMonth: "January",
-        startYear: "2026",
-        title: "Backend Web Developer",
-      },
-      {
-        isShow: true,
-        city: "New york",
-        company: "Acme company",
-        endMonth: "December",
-        endYear: "2026",
-        startMonth: "January",
-        startYear: "2026",
-        title: "Backend Web Developer",
-      },
-      {
-        isShow: true,
-        city: "New york",
-        company: "Acme company",
-        endMonth: "December",
-        endYear: "2026",
-        startMonth: "January",
-        startYear: "2026",
-        title: "Backend Web Developer",
-      },
-    ],
-    languages: [
-      { isShow: true, language: "English", level: "A1" },
-      { isShow: true, language: "English", level: "A1" },
-      { isShow: true, language: "English", level: "A1" },
-    ],
-  }
+
+  const tempResume = mockResume
 
   return (
     <div className="flex flex-col justify-center gap-y-8 my-12">
