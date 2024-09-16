3f2170912c2705585ec3cc92ac2d74e8a77586b0
brkunver <43881727+brkunver@users.noreply.github.com>
2024-09-16 15:13:56 +0300

    create biosection for pdf1 template

---
+++ src/PdfTemplates/Template1/Components/BioSection.tsx
@@ -0,0 +1,17 @@
+import { TResume } from "@/lib/types"
+import { Text, View } from "@react-pdf/renderer"
+
+function BioSection({ resume }: { resume: TResume }) {
+  return (
+    <View style={{ display: "flex", flexDirection: "column", marginTop: 20 }}>
+      <View id="name" style={{ flexDirection: "row", gap: 10 }}>
+        <Text style={{ fontSize: 22, fontWeight: "bold" }}>{resume.firstName}</Text>
+        <Text style={{ fontSize: 22, fontWeight: "light" }}>{resume.lastName}</Text>
+      </View>
+      <Text style={{ fontSize: 16, color: "#666" }}>{resume.title}</Text>
+      <Text style={{ fontSize: 12, color: "#444", marginTop : 10 }}>{resume.about}</Text>
+    </View>
+  )
+}
+
+export default BioSection
